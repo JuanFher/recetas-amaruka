7f2663538eafa59f50d3dbd467343369329ba826
JuanFher <informacion.smartpc@gmail.com>
2020-03-11 20:35:50 -0500

    first commit

---
+++ database/factories/LaboratoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Laboratory;
+use Faker\Generator as Faker;
+
+$factory->define(Laboratory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
